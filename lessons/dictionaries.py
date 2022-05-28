975ffa470b53247327f506fd68c68ca60d2833e7
James Mwendwa <mwendwajames32@gmail.com>
2022-05-28 16:35:09 +0300

    dictionaries

---
+++ lessons/dictionaries.py
@@ -0,0 +1,15 @@
+# same as javascript object notation
+
+teams = {
+    'football': 'PSG', 
+    'NBA': 'Lakers'
+}
+
+teams['football']
+
+# to check if a key exists in a dictionary, we use 'key in dict'
+
+'Bucks' in teams # returns false
+
+# you can get by full list of the keys using .keys() method
+teams.keys()
