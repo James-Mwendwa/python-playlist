6af15f7e7833043a2bf4b29aae1b73b24359063b
James Mwendwa <mwendwajames32@gmail.com>
2022-06-05 00:04:53 +0300

    sorting & sets

--- lessons/sorting.py
+++ lessons/sorting.py
@@ -1,4 +1,8 @@
-numbers = [1,2,3,9,5,5,6,8]
+numbers = [1,2,2,3,9,5,5,6,8]
 
 ## we use sorted() method
 sorted(numbers)
+
+## SETS => Do not allow duplicates
+## Sets do not preserve order
+set(numbers)
