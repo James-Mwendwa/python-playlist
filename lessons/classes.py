ea8cef782ba9aca0a666e2b18d7f85e0346944c2
James Mwendwa <mwendwajames32@gmail.com>
2022-12-09 22:01:49 +0300

    (init-function): create several instances

--- lessons/classes.py
+++ lessons/classes.py
@@ -1,16 +1,22 @@
 class Planet:
     ## create attributes
-    def __init__(self):
-        self.name = 'Saturn'
-        self.radius = 1620000
-        self.gravity = 7.3
-        self.system = 'Saturn System'
+    def __init__(self, name, radius, gravity, system):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
 
     def orbit(self):
-        return f' {self.name} is orbiting around {self.system} '    
+        return f" {self.name} is orbiting around {self.system} "
+
 
 ## new instance of the class
-hoth = Planet()
-print(f'Name is {hoth.name} and its gravity is {hoth.gravity} ')
+hoth = Planet("Saturn", 1620000, 7.3, "Saturn System")
+print(f"Name is {hoth.name} and its gravity is {hoth.gravity} ")
 
 print(hoth.orbit())
+
+
+pluto = Planet("Pluto", 400000, 3.2, "Pluto System")
+
+print(f"Name: {pluto.name}")
